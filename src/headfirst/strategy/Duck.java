f4823d90147a9865b38ca0449f424782f9aa0971
baejeongeun <wjddms5810@naver.com>
2016-03-16 17:58:10 +0900

    messge1

---
+++ src/headfirst/strategy/Duck.java
@@ -0,0 +1,11 @@
+package headfirst.strategy;
+
+public abstract class Duck{
+  FlyBehavior flyBehavior;
+  QuackBevavior quackBevavio;
+  public Duck() {}
+  public void FlyBehavio(FlyBehavior fb) {
+    flyBehavior= fb;
+  }
+  public
+  
