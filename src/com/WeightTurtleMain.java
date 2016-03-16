c5d9fec02723351cb9514c8aff3d40f67c2a2a02
baejeongeun <wjddms5810@naver.com>
2016-03-16 17:05:24 +0900

    messge

---
+++ src/com/WeightTurtleMain.java
@@ -0,0 +1,14 @@
+package com.sd.tutle;
+
+import java.awl.Color;
+import cg.aplu.turtle.*;
+
+public class WeightTurtleMain{
+  public static void main(String[] args){
+    weightTurtle wt= new Weight tutle(10);
+    System.out.println("hellow WeightTurtle");
+    wt.addSttatus(10);
+    String msg="hello ! weight " = wt.getWeight() + "kg";
+    wt.setStatusText(msg);
+  }
+}
---
+++ src/com/sd/tutle/WeightTurtle.java
@@ -0,0 +1,23 @@
+/*
+ * @auther sch
+ * @since 20160314
+ * */
+
+package com.sd.turtle;
+import ch.aplu.turtle.*;
+
+public class WeightTurtle extents Tutle{
+  Turtle t1 = new Turtle();
+  private int weigh;
+  public WeightTutle(int w){
+   weight = w;
+   
+    
+  }
+    +getWeight().int
+      
+      
+  public static void main(String[] args) {
+    new TurtleDemoMain();
+  }
+}
