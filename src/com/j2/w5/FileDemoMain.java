506afec3a9c024cb09339d9c84eb0a49d54a30a8
sch <Sch841@@naver.com>
2016-04-06 13:58:18 +0900

    aa

--- src/com/j2/w5/FileDemoMain.java
+++ src/com/j2/w5/FileDemoMain.java
@@ -5,12 +5,17 @@
  */
 
 package com.j2.w5;
-import java.io.File;
 import java.io.FileInputStream;
-
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;;
+import java.io.Reader;
+import java.io.File;
 
 public class FileDemoMain {
     public static void main(String[] args) throws Exception {
+
+        
        File myhome=new File(System.getProperty("user.home"));
        System.out.println("myhome is "+myhome);
        
@@ -36,6 +41,31 @@ public class FileDemoMain {
         }finally{
             if(isReadme != null)
                 isReadme.close();
+        }        
+        
+       File readmeUTF=new File(cwd,"UTF.txt");
+       System.out.println("readme file is "+readmeUTF);
+       
+
+        
+        InputStream isReadmeUTF = null;
+        InputStreamReader isrReadmeUTF = null;
+        BufferedReader brReadmeUTF=null;;
+        isReadmeUTF=new FileInputStream(readmeUTF);
+        isrReadmeUTF=new InputStreamReader(isReadmeUTF,"UTF8");
+        brReadmeUTF= new BufferedReader(isrReadmeUTF);
+            String str;
+        try{
+       
+
+            while((str=brReadmeUTF.readLine())!=null) {
+                System.out.println("--"+str);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            if(brReadmeUTF != null)
+                brReadmeUTF.close();
         }
     }
 }
